747e90e29b52a87c497182be58b91912ca6200f3
DaivdZhang <DaivdZhang@users.noreply.github.com>
2016-07-04 13:34:43 +0800

    Update matrix.py

--- src/mathematics/matrix.py
+++ src/mathematics/matrix.py
@@ -4,11 +4,11 @@ from functools import reduce
 
 
 class Matrix(object):
-    def __init__(self, mat=None):
-        if mat is None:
-            self.matrix = []
+    def __init__(self, array=None):
+        if array is None:
+            self.array = []
         else:
-            self.matrix = mat
+            self.array = array
         self.shape = self.get_shape()
 
     def __add__(self, other):
@@ -23,14 +23,14 @@ class Matrix(object):
             return list3
 
         for i in range(self.shape[0]):
-            result += [_add(self.matrix[i], other.matrix[i])]
+            result += [_add(self.array[i], other.array[i])]
         return Matrix(result)
 
     def __mul__(self, other):
         def n_mul(mat, n):
             result = []
             for i in range(mat.get_shape()[0]):
-                result.append([n * x for x in mat.matrix[i]])
+                result.append([n * x for x in mat.array[i]])
             return result
 
         def m_mul(mat1, mat2):
@@ -48,7 +48,7 @@ class Matrix(object):
             mat2 = mat2.t()
             for i in range(mat1.shape[0]):
                 for j in range(mat1.shape[1]):
-                    temp += [_mul(mat1.matrix[i], mat2.matrix[j])]
+                    temp += [_mul(mat1.array[i], mat2.array[j])]
                 result.append(temp)
                 temp = []
             else:
@@ -64,64 +64,66 @@ class Matrix(object):
         return self + other
 
     def t(self):  # TODO: speed up transpose function
-        t_matrix = []
-        if not self.matrix:
+        if not self.array:
             return []
-        for j in range(self.shape[1]):
-            t_matrix.append([self.matrix[i][j] for i in range(self.shape[0])])
-        return Matrix(t_matrix)
+        t_array = [[row[j] for row in self.array] for j in range(self.shape[1])]
+        return Matrix(t_array)
 
     def inv(self):  # TODO: finish the inv function
-        pass
-
-    @staticmethod
-    def det(mat):
-        if mat.shape[0] != mat.shape[1]:
+        if self.shape[0] != self.shape[1]:
             raise IndexError
 
-        for j in range(mat.shape[0]):
-            for i in range(j+1, mat.shape[0]):
-                k = mat.matrix[i][j]/mat.matrix[j][j]
-                mat.matrix[i] = list(map(lambda x, y: y - k*x, mat.matrix[j], mat.matrix[i]))
-        main_diagonal = map(lambda x: mat.get(x, x), [row for row in range(mat.shape[0])])
+    def det(self):
+        if self.shape[0] != self.shape[1]:
+            raise IndexError
+
+        for j in range(self.shape[0]):
+            for i in range(j+1, self.shape[0]):
+                k = self.array[i][j]/self.array[j][j]
+                self.array[i] = list(map(lambda x, y: y - k*x, self.array[j], self.array[i]))
+        main_diagonal = map(lambda x: self.get(x, x), [row for row in range(self.shape[0])])
         return reduce(lambda x, y: x*y, main_diagonal)
 
     def get_shape(self):
-        if self.matrix:
-            return len(self.matrix), len(self.matrix[0])
+        if self.array:
+            return len(self.array), len(self.array[0])
         else:
             return None
 
-    def zero(self, row=3, col=3):
+    @classmethod
+    def zero(cls, row=3, col=3):
+        array = []
         for y in range(row):
-            self.matrix += [[0*x for x in range(col)]]
-        self.shape = (row, col)
+            array += [[0 for x in range(col)]]
+        return cls(array)
 
-    def eye(self, n=2):
-        self.zero(row=n, col=n)
+    @classmethod
+    def eye(cls, n=2):
+        mat = cls.zero(row=n, col=n)
         for i in range(n):
-            self.matrix[i][i] = 1
-        self.shape = self.get_shape()
+            mat.array[i][i] = 1
+        return mat
 
-    def rand(self, row, col):
+    @classmethod
+    def rand(cls, row, col):
         random.seed(time.time())
-        self.zero(row, col)
-        for i in range(self.get_shape()[0]):
-            self.matrix[i] = list(map(lambda x: x + random.random(), self.matrix[i]))
-        self.shape = self.get_shape()
+        mat = cls.zero(row, col)
+        for i in range(row):
+            mat.array[i] = list(map(lambda x: x + random.random(), mat.array[i]))
+        return mat
 
     def get(self, row, col):
-        if row >= self.get_shape()[0] or col >= self.get_shape()[1]:
+        if row >= self.shape[0] or col >= self.shape[1]:
             raise IndexError
-        return self.matrix[row][col]
+        return self.array[row][col]
 
     def mprint(self):
-        for i in range(self.get_shape()[0]):
+        for i in range(self.shape[0]):
             if i == 0:
                 print('[')
-                print(self.matrix[i])
-            elif i == self.get_shape()[0] - 1:
-                print(self.matrix[i])
+                print(self.array[i])
+            elif i == self.shape[0] - 1:
+                print(self.array[i])
                 print(']')
             else:
-                print(self.matrix[i])
+                print(self.array[i])
