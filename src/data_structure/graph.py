346107188a403d14d84f07924484033b3b4b4cb3
akizunx <17682291+akizunx@users.noreply.github.com>
2016-10-20 11:47:48 +0800

    Update graph.py

--- src/data_structure/graph.py
+++ src/data_structure/graph.py
@@ -1,6 +1,10 @@
 from copy import deepcopy as _deepcopy
 
 
+class GraphError(Exception):
+    pass
+
+
 class Graph(object):
     def __init__(self, matrix, unconnected=0):
         self.vertex_num = len(matrix)
@@ -25,14 +29,50 @@ class Graph(object):
         return self.matrix[vi][vj]
 
     def out_edges(self, vi):
-        return self._out_edges(vi, self.unconnected)
+        return self._out_edges(self.matrix, vi, self.unconnected)
 
-    def _out_edges(self, vi, unconnected):
-        return [(i, value) for i, value in enumerate(self.matrix[vi]) if value != unconnected]
+    @staticmethod
+    def _out_edges(matrix, vi, unconnected):
+        return [(i, value) for i, value in enumerate(matrix[vi]) if value != unconnected]
 
-    def add_vertex(self, unconnected=self.unconnected):
+    def add_vertex(self, unconnected=0):
         new_row = [unconnected]*self.vertex_num
         self.matrix.append(new_row)
         self.vertex_num = self._get_vnum()
         for row in self.matrix:
             row.append(unconnected)
+
+
+class GraphAL(Graph):
+    def __init__(self, matrix, unconnected=0):
+        super().__init__(matrix, unconnected)
+        self.matrix = [Graph.out_edges(self, i) for i in range(self.vertex_num)]
+
+    def add_vertex(self, unconnected=0):
+        self.matrix.append([])
+        self.vertex_num = self._get_vnum()
+        return self.vertex_num - 1
+
+    def add_edge(self, vi, vj, value=1):
+        if self.vertex_num == 0:
+            raise GraphError("cannot add edge to a empty graph")
+
+        i = 0
+        row = self.matrix[vi]
+        for i, _ in enumerate(row):
+            if row[i][0] == vj:
+                self.matrix[vi][i] = (vj, value)
+                return None
+            if row[i][0] > vj:
+                break
+        self.matrix[vi].insert(i+1, (vj, value))
+
+    def get_edge(self, vi, vj):
+        for i, value in self.matrix[vi]:
+            if i == vj:
+                return value
+        else:
+            return self.unconnected
+
+    def out_edges(self, vi):
+        return self.matrix[vi]
